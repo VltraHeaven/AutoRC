2cecbf558c2f7fd411192598d74957e51bdbb967
Julio Hawthorne <julio.hawthorne@carvana.com>
2021-01-02 07:15:55 -0700

    Various additions to enhance stability
    - Set ChromeDriver options to start browser maximized
    - Increased implicit wait
    - Modified and added wait_until function calls to account for webapp slow loading
    - Modified argparse implementation to make room for future arguments
    - Added conditionals to wait until elements exist/are enabled
    - Modified and added calls to time.sleep to account for slowness in loading times.
    - Added kill_browser and sys.exit() at end to gracefully exit script.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 import time
 import argparse
 import sys
-
+from selenium.webdriver import ChromeOptions
 
 if __name__ == "__main__":
 
@@ -13,26 +13,31 @@ if __name__ == "__main__":
             usage="Usage: " + sys.argv[0] + " [FILE]...",
             description="Description: Needs path to the user list .csv as an argument. e.g. 'python main.py $home\\Documents\\userlist.csv'\nThe .csv must have the following case-sensitive headers: givenName,surname,name,emailAddress,Title\n"
         )
-        parser.add_argument('files', nargs='*')
+        parser.add_argument('files')
         return parser
 
-    aparser = init_argparse()
-    listpath = aparser.parse_args()
+    init_args = init_argparse()
+    aparser = init_args.parse_args()
 
-    if not listpath.files:
+    if not aparser.files:
         print(aparser.usage)
         print(aparser.description)
     else:
+        # WebDriver setting
+        options = ChromeOptions()
+        options.add_argument('--start-maximized')
+
         # Helium Config
-        Config.implicit_wait_secs = 15
-        start_chrome('https://login.ringcentral.com/')
+        Config.implicit_wait_secs = 60
+        start_chrome('https://login.ringcentral.com/', options=options)
+        wait_until(Text("Single Sign-on").exists)
         click('Single Sign-on')
 
         print('Enter your email address in the browser and click "Submit" to log into RingCentral.')
         print('This script will continue when you have successfully accessed the Admin Portal.')
         wait_until(Text("Admin Portal").exists, timeout_secs=120, interval_secs=.5)
 
-        with open(listpath.files) as userlist:
+        with open(aparser.files) as userlist:
             reader = csv.DictReader(userlist)
             for row in reader:
                 go_to('https://service.ringcentral.com/application/users/users/unassigned')
@@ -43,16 +48,24 @@ if __name__ == "__main__":
                 nhtitle = row['Title']
                 print('Assigning ' + nhdisplayname + ' a RingCentral Extension, please wait...')
                 wait_until(lambda: not Text('Loading...').exists())
+                time.sleep(5)
                 write('app', into='Search Users')
                 press(ENTER)
+                wait_until(Text("Ext. with RingCentral Phone app").exists)
                 time.sleep(5)
                 click('Ext. with RingCentral Phone app')
                 write(nhemail, into='Email Address')
                 write(nhfirstname, into='First Name')
                 write(nhlastname, into='Last Name')
                 write(nhtitle, into='Job Title')
-                click('Verify Email Uniqueness')
+                if Button('Verify Email Uniqueness').is_enabled():
+                    click(Button('Verify Email Uniqueness'))
+                else:
+                    print(nhdisplayname + ' <' + nhemail + '>' + ' is not a valid email address. Skipping...')
+                    continue
+
                 wait_until(lambda: not Text('Loading...').exists())
+
                 if Text('Duplicate Email Association').exists():
                     print(nhdisplayname + ' already has an assigned RingCentral extension')
                     continue
@@ -68,7 +81,7 @@ if __name__ == "__main__":
                 ext = Text(to_right_of='Phone Number:').value
                 time.sleep(5)
                 click(Text('Save'))
-                time.sleep(10)
+                time.sleep(5)
                 wait_until(lambda: not Text('Loading...').exists())
                 go_to('https://service.ringcentral.com/application/users/users/default')
                 wait_until(lambda: not Text('Loading...').exists())
@@ -76,6 +89,8 @@ if __name__ == "__main__":
                 write('', into='Search Users')
                 write(nhdisplayname, into='Search Users')
                 press(ENTER)
+                wait_until(Button(nhdisplayname, to_left_of=ext).is_enabled, timeout_secs=120, interval_secs=.5)
+                time.sleep(5)
                 click(Button(nhdisplayname, to_left_of=ext))
                 wait_until(lambda: not Text('Loading...').exists())
                 click('Call Handling & Forwarding')
@@ -84,3 +99,6 @@ if __name__ == "__main__":
                 click(Text('Save'))
                 print(nhdisplayname + ' has been assigned RingCentral Extension: ' + ext)
                 del ext
+        print('RingCentral accounts created successfully.')
+        kill_browser()
+        sys.exit(0)

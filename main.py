f67b328c2eab9e528899212e08e9a9b17516d390
Julio Hawthorne <julio.hawthorne@carvana.com>
2021-01-02 10:02:47 -0700

    Moved core functionality to func.py imports
    - Moved core functionality to functions in func.py
    - Added func import
    - Added check for assigned extension to catch invalid email addresses.
    - Splitting core functionality to assign and set_fward functions configures extended Ring For settings even if the user has a pre-existing assigned extension.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from helium import *
 import csv
-import time
 import argparse
 import sys
 from selenium.webdriver import ChromeOptions
+from func import assign, set_fward
 
 if __name__ == "__main__":
 
@@ -15,6 +15,7 @@ if __name__ == "__main__":
         )
         parser.add_argument('files')
         return parser
+
 
     init_args = init_argparse()
     aparser = init_args.parse_args()
@@ -40,65 +41,15 @@ if __name__ == "__main__":
         with open(aparser.files) as userlist:
             reader = csv.DictReader(userlist)
             for row in reader:
-                go_to('https://service.ringcentral.com/application/users/users/unassigned')
                 nhfirstname = row['givenName']
                 nhlastname = row['surname']
                 nhdisplayname = row['name']
                 nhemail = row['emailAddress']
                 nhtitle = row['Title']
-                print('Assigning ' + nhdisplayname + ' a RingCentral Extension, please wait...')
-                wait_until(lambda: not Text('Loading...').exists())
-                time.sleep(5)
-                write('app', into='Search Users')
-                press(ENTER)
-                wait_until(Text("Ext. with RingCentral Phone app").exists)
-                time.sleep(5)
-                click('Ext. with RingCentral Phone app')
-                write(nhemail, into='Email Address')
-                write(nhfirstname, into='First Name')
-                write(nhlastname, into='Last Name')
-                write(nhtitle, into='Job Title')
-                if Button('Verify Email Uniqueness').is_enabled():
-                    click(Button('Verify Email Uniqueness'))
-                else:
-                    print(nhdisplayname + ' <' + nhemail + '>' + ' is not a valid email address. Skipping...')
-                    continue
-
-                wait_until(lambda: not Text('Loading...').exists())
-
-                if Text('Duplicate Email Association').exists():
-                    print(nhdisplayname + ' already has an assigned RingCentral extension')
-                    continue
-                else:
-                    click(Button('OK'))
-
-                if not RadioButton('Send Invite').is_selected():
-                    click(RadioButton('Send Invite'))
-
-                if CheckBox('Yes, I would like to receive information on product education, training materials, etc').is_checked():
-                    click(CheckBox('Yes, I would like to receive information on product education, training materials, etc'))
-
-                ext = Text(to_right_of='Phone Number:').value
-                time.sleep(5)
-                click(Text('Save'))
-                time.sleep(5)
-                wait_until(lambda: not Text('Loading...').exists())
-                go_to('https://service.ringcentral.com/application/users/users/default')
-                wait_until(lambda: not Text('Loading...').exists())
-                time.sleep(5)
-                write('', into='Search Users')
-                write(nhdisplayname, into='Search Users')
-                press(ENTER)
-                wait_until(Button(nhdisplayname, to_left_of=ext).is_enabled, timeout_secs=120, interval_secs=.5)
-                time.sleep(5)
-                click(Button(nhdisplayname, to_left_of=ext))
-                wait_until(lambda: not Text('Loading...').exists())
-                click('Call Handling & Forwarding')
-                wait_until(lambda: not Text('Loading...').exists())
-                select(ComboBox('Ring For'), '15 Rings / 75 Secs')
-                click(Text('Save'))
-                print(nhdisplayname + ' has been assigned RingCentral Extension: ' + ext)
-                del ext
+                assignedext = assign(nhfirstname, nhlastname, nhdisplayname, nhemail, nhtitle)
+                if assignedext is not None:
+                    set_fward(nhdisplayname, assignedext)
+                del assignedext
         print('RingCentral accounts created successfully.')
         kill_browser()
         sys.exit(0)

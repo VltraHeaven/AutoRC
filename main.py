080ed649553fa03e7b6cdfd732543a1664067f0b
Julio Hawthorne <julio.hawthorne@carvana.com>
2021-01-08 08:11:17 -0700

    Major Refactor
    - Removed major functionality to individual functions in func.py
    - Added account removal functionality
    - Modified command line arguments
    - Created class Users to initialize and open the userlist at runtime
    - Set extension assignment and removal operations to methods of class Users.

--- main.py
+++ main.py
@@ -2,43 +2,16 @@ from helium import *
 import csv
 import argparse
 import sys
-from selenium.webdriver import ChromeOptions
-from func import assign, set_fward
-
-if __name__ == "__main__":
-
-    # Import userlist.csv and save to dictionary
-    def init_argparse() -> argparse.ArgumentParser:
-        parser = argparse.ArgumentParser(
-            usage="Usage: " + sys.argv[0] + " [FILE]...",
-            description="Description: Needs path to the user list .csv as an argument. e.g. 'python main.py $home\\Documents\\userlist.csv'\nThe .csv must have the following case-sensitive headers: givenName,surname,name,emailAddress,Title\n"
-        )
-        parser.add_argument('files')
-        return parser
+from func import delete, assign, set_fward, login
 
 
-    init_args = init_argparse()
-    aparser = init_args.parse_args()
+class Users:
+    def __init__(self, filepath):
+        self.filepath = filepath
 
-    if not aparser.files:
-        print(aparser.usage)
-        print(aparser.description)
-    else:
-        # WebDriver setting
-        options = ChromeOptions()
-        options.add_argument('--start-maximized')
-
-        # Helium Config
-        Config.implicit_wait_secs = 60
-        start_chrome('https://login.ringcentral.com/', options=options)
-        wait_until(Text("Single Sign-on").exists)
-        click('Single Sign-on')
-
-        print('Enter your email address in the browser and click "Submit" to log into RingCentral.')
-        print('This script will continue when you have successfully accessed the Admin Portal.')
-        wait_until(Text("Admin Portal").exists, timeout_secs=120, interval_secs=.5)
-
-        with open(aparser.files) as userlist:
+    def newext(self):
+        login()
+        with open(self.filepath) as userlist:
             reader = csv.DictReader(userlist)
             for row in reader:
                 nhfirstname = row['givenName']
@@ -49,7 +22,61 @@ if __name__ == "__main__":
                 assignedext = assign(nhfirstname, nhlastname, nhdisplayname, nhemail, nhtitle)
                 if assignedext is not None:
                     set_fward(nhdisplayname, assignedext)
-                del assignedext
+                    del assignedext
+                print('Extension assignment and configuration for ' + nhdisplayname + ' complete.')
         print('RingCentral accounts created successfully.')
         kill_browser()
         sys.exit(0)
+
+    def delext(self):
+        login()
+        with open(self.filepath) as userlist:
+            reader = csv.DictReader(userlist)
+            for row in reader:
+                nhfirstname = row['givenName']
+                nhlastname = row['surname']
+                nhdisplayname = row['name']
+                nhemail = row['emailAddress']
+                nhtitle = row['Title']
+                delete(nhfirstname, nhlastname, nhdisplayname, nhemail, nhtitle)
+                print('Extension removal for ' + nhdisplayname + ' complete.')
+        print('RingCentral accounts successfully removed.')
+        kill_browser()
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+
+    def init_argparse() -> argparse.ArgumentParser:
+        parser = argparse.ArgumentParser(
+            usage="Usage: " + sys.argv[0] + " [-h] [-a | -r] [file]",
+            description="Description: Automatically assign or remove RingCentral extensions."
+        )
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('-a', '--assign', help='Assign RingCentral extensions', action='store_true')
+        group.add_argument('-r', '--remove', help='Remove RingCentral extension assignments', action='store_true')
+        parser.add_argument('file',
+                            help='Path to the userlist .csv file.\nThe .csv must at least have the following '
+                                 'case-sensitive headers: givenName,surname,name,emailAddress,Title\n"')
+        return parser
+
+
+    init_args = init_argparse()
+    aparser = init_args.parse_args()
+
+    if aparser.assign:
+        if not aparser.file:
+            print(aparser.usage)
+            print(aparser.description)
+        else:
+            assign_init = Users(aparser.file)
+            assign_init.newext()
+    elif aparser.remove:
+        if not aparser.file:
+            print(aparser.usage)
+            print(aparser.description)
+        else:
+            remove_init = Users(aparser.file)
+            remove_init.delext()
+    else:
+        print(aparser.usage + '\n' + aparser.description)

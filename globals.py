91b7c8f65d8cdc68e3273e84e21ffaf7dd2f8bb1
Julio Hawthorne <julio.hawthorne@carvana.com>
2021-02-13 06:31:40 -0700

    Refactor, bugfixes to align with RingCentral admin portal frontend changes
    - Removed login() function and moved webdriver initialization into Users class
    -  Fixed Search text entry field detection globally
    - Fixed extension and registered full name scraping in remove.py  
    - Element detection fixes for forwarding settings in assign.py

--- globals.py
+++ globals.py
@@ -1,28 +1,6 @@
 from helium import *
 import time
-from selenium.webdriver import ChromeOptions
 from selenium.common import exceptions
-
-
-def login():
-    # WebDriver setting
-    options = ChromeOptions()
-    options.add_argument('--start-maximized')
-    # Helium Config
-    Config.implicit_wait_secs = 240
-    start_chrome('https://service.ringcentral.com/', options=options)
-    try:
-        wait_until(Text("Single Sign-on").exists)
-        click('Single Sign-on')
-    except exceptions.TimeoutException or exceptions.StaleElementReferenceException or exceptions.NoSuchElementException:
-        print('Click the Single Sign-on button, enter your email address in the browser and click "Submit" to log '
-              'into RingCentral.')
-        time.sleep(2)
-    else:
-        print('Enter your email address in the browser and click "Submit" to log into RingCentral.')
-    finally:
-        print('This script will continue when you have successfully accessed the Admin Portal.')
-    wait_until(Text("Admin").exists, timeout_secs=120, interval_secs=.5)
 
 
 def nav_assigned():
